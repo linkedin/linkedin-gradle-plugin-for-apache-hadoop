bae9588446aacba762f47c2c2bf87b68386f7dfb
Alex Bain <abain@linkedin.com>
2014-07-18 17:20:14 -0700

    Add factory methods to the HadoopPlugin class to make the AzkabanPlugin and PigPlugin implementation classes overrideable by subclasses

--- hadoop-plugin/src/main/groovy/com/linkedin/gradle/hadoop/HadoopPlugin.groovy
+++ hadoop-plugin/src/main/groovy/com/linkedin/gradle/hadoop/HadoopPlugin.groovy
@@ -12,7 +12,17 @@ import org.gradle.api.Project;
 class HadoopPlugin implements Plugin<Project> {
 
   void apply(Project project) {
-    project.getPlugins().apply(AzkabanPlugin.class);
-    project.getPlugins().apply(PigPlugin.class);
+    project.getPlugins().apply(getAzkabanPluginClass());
+    project.getPlugins().apply(getPigPluginClass());
+  }
+
+  // Factory method to return the AzkabanPlugin class that can be overridden by subclasses.
+  Class getAzkabanPluginClass() {
+    return AzkabanPlugin.class;
+  }
+
+  // Factory method to return the PigPlugin class that can be overridden by subclasses.
+  Class getPigPluginClass() {
+    return PigPlugin.class;
   }
 }
