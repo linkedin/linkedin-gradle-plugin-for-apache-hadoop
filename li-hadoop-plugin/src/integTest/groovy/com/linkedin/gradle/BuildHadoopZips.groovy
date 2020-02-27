801b5870ce71a06d6e1ece30811e255bf5e139fb
Suren Nihalani <1093911+SurenNihalani@users.noreply.github.com>
2020-02-27 07:26:10 -0800

    ScmPlugin: Try to generate buildMetaData.json even when scmPlugin is disabled
    People seem to disable scm plugin to improve their zip size/performance.
    Changed the flag to two flags to allow metadata.json to be created by
    default but for projects where existing flags is set to false, the zip will not be created

--- li-hadoop-plugin/src/integTest/groovy/com/linkedin/gradle/BuildHadoopZips.groovy
+++ li-hadoop-plugin/src/integTest/groovy/com/linkedin/gradle/BuildHadoopZips.groovy
@@ -12,58 +12,131 @@ import java.util.zip.ZipFile
 
 class BuildHadoopZips extends Specification {
 
-    @Rule
-    TemporaryFolder tmp = new TemporaryFolder()
+  @Rule
+  TemporaryFolder tmp = new TemporaryFolder()
 
-    def buildDotGradle
-    def settingsDotGradle
-    def gradleDotProperties
-    def dotCrt
+  def buildDotGradle
+  def settingsDotGradle
+  def gradleDotProperties
+  def dotCrt
 
-    def setup() {
-        tmp.create()
-        buildDotGradle = tmp.newFile('build.gradle')
-        settingsDotGradle = tmp.newFile('settings.gradle')
-        gradleDotProperties = tmp.newFile('gradle.properties')
-        dotCrt = tmp.newFile('.crt')
-    }
+  def setup() {
+    tmp.create()
+    buildDotGradle = tmp.newFile('build.gradle')
+    settingsDotGradle = tmp.newFile('settings.gradle')
+    gradleDotProperties = tmp.newFile('gradle.properties')
+    dotCrt = tmp.newFile('.crt')
+  }
 
-    /**
-     * Integration test for including the sources zip and SCM metadata file in the Hadoop zip
-     */
-    def 'verify zip contents'() {
-        given:
-        def projectName = 'build-hadoop-zips'
-        def version = '1.0.0'
-        buildDotGradle << this.class.classLoader.getResource('buildZips/buildZipsCRT.gradle').text
-        settingsDotGradle << """rootProject.name='${projectName}'"""
-        gradleDotProperties << """version=${version}"""
-        dotCrt << '''Test .crt file\n'''
-        GradleRunner runner = GradleRunner.create()
-                .withProjectDir(tmp.root)
-                .withPluginClasspath()
-                .withArguments('buildHadoopZips', '-is')
+  /**
+   * Integration test for including the sources zip and SCM metadata file in the Hadoop zip*/
+  def 'verify zip contents'() {
+    given:
+    def projectName = 'build-hadoop-zips'
+    def version = '1.0.0'
+    buildDotGradle << this.class.classLoader.getResource('buildZips/buildZipsCRT.gradle').text
+    settingsDotGradle << """rootProject.name='${projectName}'"""
+    gradleDotProperties << """version=${version}"""
+    dotCrt << '''Test .crt file\n'''
+    GradleRunner runner = GradleRunner.create()
+        .withProjectDir(tmp.root)
+        .withPluginClasspath()
+        .withArguments('buildHadoopZips', '-is')
 
-        when:
-        BuildResult result = runner.build()
+    when:
+    BuildResult result = runner.build()
 
-        then:
-        result.task(':azkabanHadoopZip').outcome == TaskOutcome.SUCCESS
-        result.task(':CRTHadoopZip').outcome == TaskOutcome.SUCCESS
+    then:
+    result.task(':azkabanHadoopZip').outcome == TaskOutcome.SUCCESS
+    result.task(':CRTHadoopZip').outcome == TaskOutcome.SUCCESS
 
-        // find CRTHadoopZip, assert contains .crt file (build/distributions/build-hadoop-zips-1.0.0.zip)
-        def crtHadoopZip = new File(tmp.root, "build/distributions/${projectName}-${version}.zip")
-        crtHadoopZip.exists()
-        def crtHadoopZipContents = new ZipFile(crtHadoopZip)
-        assert crtHadoopZipContents.getEntry('.crt') != null
+    // find CRTHadoopZip, assert contains .crt file (build/distributions/build-hadoop-zips-1.0.0.zip)
+    def crtHadoopZip = new File(tmp.root, "build/distributions/${projectName}-${version}.zip")
+    crtHadoopZip.exists()
+    def crtHadoopZipContents = new ZipFile(crtHadoopZip)
+    assert crtHadoopZipContents.getEntry('.crt') != null
 
-        // find azkabanHadoopZip, assert contents (build/distributions/build-hadoop-zips-1.0.0-azkaban.zip)
-        //  contents == build.gradle, buildMetadata.json, build-hadoop-zips-1.0.0-sources.zip
-        def azkabanZip = new File(tmp.root, "build/distributions/${projectName}-${version}-azkaban.zip")
-        azkabanZip.exists()
-        def zipFileContents = new ZipFile(azkabanZip)
-        zipFileContents.getEntry('build.gradle') != null
-        zipFileContents.getEntry('buildMetadata.json') != null
-        zipFileContents.getEntry("${projectName}-${version}-sources.zip") != null
-    }
+    // find azkabanHadoopZip, assert contents (build/distributions/build-hadoop-zips-1.0.0-azkaban.zip)
+    //  contents == build.gradle, buildMetadata.json, build-hadoop-zips-1.0.0-sources.zip
+    def azkabanZip = new File(tmp.root, "build/distributions/${projectName}-${version}-azkaban.zip")
+    azkabanZip.exists()
+    def zipFileContents = new ZipFile(azkabanZip)
+    zipFileContents.getEntry('build.gradle') != null
+    zipFileContents.getEntry('buildMetadata.json') != null
+    zipFileContents.getEntry("${projectName}-${version}-sources.zip") != null
+  }
+
+  def 'verify zip contents doesnt include buildMetadata when asked'() {
+    given:
+    def projectName = 'build-hadoop-zips'
+    def version = '1.0.0'
+    buildDotGradle << this.class.classLoader.getResource('buildZips/buildZipsCRT.gradle').text
+    settingsDotGradle << """rootProject.name='${projectName}'"""
+    gradleDotProperties << """version=${version}\ndisableScmPluginMetaData=true\n"""
+    dotCrt << '''Test .crt file\n'''
+    GradleRunner runner = GradleRunner.create()
+        .withProjectDir(tmp.root)
+        .withPluginClasspath()
+        .withArguments('buildHadoopZips', '-is')
+        .forwardOutput()
+        .withDebug(true)
+
+    when:
+    BuildResult result = runner.build()
+
+    then:
+    result.task(':azkabanHadoopZip').outcome == TaskOutcome.SUCCESS
+    result.task(':CRTHadoopZip').outcome == TaskOutcome.SUCCESS
+
+    // find CRTHadoopZip, assert contains .crt file (build/distributions/build-hadoop-zips-1.0.0.zip)
+    def crtHadoopZip = new File(tmp.root, "build/distributions/${projectName}-${version}.zip")
+    crtHadoopZip.exists()
+    def crtHadoopZipContents = new ZipFile(crtHadoopZip)
+    assert crtHadoopZipContents.getEntry('.crt') != null
+
+    // find azkabanHadoopZip, assert contents (build/distributions/build-hadoop-zips-1.0.0-azkaban.zip)
+    //  contents == build.gradle, buildMetadata.json, build-hadoop-zips-1.0.0-sources.zip
+    def azkabanZip = new File(tmp.root, "build/distributions/${projectName}-${version}-azkaban.zip")
+    azkabanZip.exists()
+    def zipFileContents = new ZipFile(azkabanZip)
+    zipFileContents.getEntry('build.gradle') != null
+    zipFileContents.getEntry('buildMetadata.json') == null
+    zipFileContents.getEntry("${projectName}-${version}-sources.zip") != null
+  }
+
+  def 'verify zip contents doesnt include buildMetadata AND sources when asked'() {
+    given:
+    def projectName = 'build-hadoop-zips'
+    def version = '1.0.0'
+    buildDotGradle << this.class.classLoader.getResource('buildZips/buildZipsCRT.gradle').text
+    settingsDotGradle << """rootProject.name='${projectName}'"""
+    gradleDotProperties << """version=${version}\ndisableScmPluginMetaData=true\ndisableScmPlugin=true"""
+    dotCrt << '''Test .crt file\n'''
+    GradleRunner runner = GradleRunner.create()
+        .withProjectDir(tmp.root)
+        .withPluginClasspath()
+        .withArguments('buildHadoopZips', '-is')
+
+    when:
+    BuildResult result = runner.build()
+
+    then:
+    result.task(':azkabanHadoopZip').outcome == TaskOutcome.SUCCESS
+    result.task(':CRTHadoopZip').outcome == TaskOutcome.SUCCESS
+
+    // find CRTHadoopZip, assert contains .crt file (build/distributions/build-hadoop-zips-1.0.0.zip)
+    def crtHadoopZip = new File(tmp.root, "build/distributions/${projectName}-${version}.zip")
+    crtHadoopZip.exists()
+    def crtHadoopZipContents = new ZipFile(crtHadoopZip)
+    assert crtHadoopZipContents.getEntry('.crt') != null
+
+    // find azkabanHadoopZip, assert contents (build/distributions/build-hadoop-zips-1.0.0-azkaban.zip)
+    //  contents == build.gradle, buildMetadata.json, build-hadoop-zips-1.0.0-sources.zip
+    def azkabanZip = new File(tmp.root, "build/distributions/${projectName}-${version}-azkaban.zip")
+    azkabanZip.exists()
+    def zipFileContents = new ZipFile(azkabanZip)
+    zipFileContents.getEntry('build.gradle') != null
+    zipFileContents.getEntry('buildMetadata.json') == null
+    zipFileContents.getEntry("${projectName}-${version}-sources.zip") == null
+  }
 }
